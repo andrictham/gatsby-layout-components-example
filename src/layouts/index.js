f884525d15b4c5412db0bb40e0df07601e8dc069
Andric Tham <andric@nuisance.co>
2017-10-06 12:45:23 +0800

    Add navigation bar to main layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,55 @@
 // This file creates a component that automatically wraps around all pages. Good for global styling.
 
 import React from 'react'
+import Link from 'gatsby-link'
 
-const containerStyle = {
-  margin: `0 auto`,
-  maxWidth: 650,
-  padding: `0 1rem`,
+const styles = {
+  container: {
+    margin: `0 auto`,
+    maxWidth: 650,
+    padding: `1.25rem 1rem`,
+  },
+  title: {
+    display: `inline`,
+  },
+  linkLi: {
+    display: `inline-block`,
+    marginRight: `1rem`,
+  },
+  homeLi: {
+    textShadow: `none`,
+    backgroundImage: `none`,
+  },
+  navUl: {
+    listStyle: `none`,
+    float: `right`,
+  },
+  header: {
+    marginBottom: `1.5rem`,
+  },
 }
+
+const ListLink = ({ to, children }) =>
+  <li style={styles.linkLi}>
+    <Link to={to}>
+      {children}
+    </Link>
+  </li>
 
 export default ({ children }) =>
 // Destructure this.props.children
-  <div style={containerStyle}>
-    <h3>MySweetSite</h3>
+  <div style={styles.container}>
+    <header style={styles.header}>
+      <Link to="/" style={styles.homeLi}>
+        <h3 style={styles.title}>
+          MySweetSite
+        </h3>
+      </Link>
+      <ul style={styles.navUl}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about">About</ListLink>
+        <ListLink to="/contact">Contact</ListLink>
+      </ul>
+    </header>
     { children() }
   </div>
